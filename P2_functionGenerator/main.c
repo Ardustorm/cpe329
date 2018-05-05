bc1f62d9e9b9336bb842b2940a8d656964b96658
Ardustorm <luke24601@gmail.com>
2018-05-04 18:59:27 -0700

    function generator added

--- P2_functionGenerator/main.c
+++ P2_functionGenerator/main.c
@@ -1,6 +1,10 @@
 #include "msp.h"
 #include "myLibs/delay.h"
 #include "waveforms.h"
+#include "myLibs/lcd.h"
+#include "myLibs/keypad.h"
+#include <math.h>
+#include <stdlib.h>
 /**
  * main.c
  * P1.5  --  Sclk
@@ -12,10 +16,15 @@
 
 #define CUR_CLOCK_FREQ FREQ_12MHz
 
-/* #define FREQ_CUR FREQ_12MHz */
+#define FREQ_CUR FREQ_12MHz
 
+
+#define SQUARE_WT 0
+#define SAWTOOTH_WT 1
+#define SINE_WT 2
+#define ARB_WT 3
 volatile int waveFreq = 1;
-volatile int waveType = 0;		/* Square:0,  Sawtooth:1, Sine:2, inverseSaw:3 */
+volatile int waveType = SQUARE_WT;		/* Square:0,  Sawtooth:1, Sine:2, inverseSaw:3 */
 volatile int waveDuty = 5;		/* in perdeci */
 
 int key;
@@ -28,87 +37,41 @@ volatile void main(void)
    
    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-   /* set_DCO(CUR_CLOCK_FREQ); */
+   set_DCO(CUR_CLOCK_FREQ);
    setupDAC();
    keypadInit();
    LCD_Init();
    /*** TIMER INTERRUPTS ****/
    TIMER_A0->CCTL[0] = TIMER_A_CCTLN_CCIE; // TACCR0 interrupt enabled
-   TIMER_A0->CCR[0] = 1240;
-   TIMER_A0->CTL = TIMER_A_CTL_SSEL__SMCLK | // SMCLK, continuous mode
-      TIMER_A_CTL_MC__CONTINUOUS;
+   TIMER_A0->CCR[0] = 50240;
+   TIMER_A0->CTL = (TIMER_A_CTL_SSEL__SMCLK | // SMCLK, continuous mode
+		    TIMER_A_CTL_MC__CONTINUOUS);
 
-   /* SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;    // Enable sleep on exit from ISR */
+
    NVIC->ISER[0] = 1 << ((TA0_0_IRQn) & 31);
 
-
+   generateArrays();
    
    __enable_irq();
    
    Clear_LCD();   
    while(1) {
 
-      Write_str_LCD(0,"Freq: ");
-      Write_char_LCD(6,'0'+waveFreq);
-      Write_str_LCD(7,"00 Hz");
+      /* Write_str_LCD(0,"Freq: "); */
+      /* Write_char_LCD(6,'0'+waveFreq); */
+      /* Write_str_LCD(7,"00 Hz"); */
 
-      if(waveType == 0) {
-	 Write_char_LCD(29, '0'+waveDuty);
-	 Write_str_LCD(30, "0%");
-      }
+      /* if(waveType == SQUARE_WT) { /\* Display Duty cycle *\/ */
+      /* 	 Write_char_LCD(29, '0'+waveDuty); */
+      /* 	 Write_str_LCD(30, "0%"); */
+      /* } */
+
       key=getKey();
-
+      updateTypeAndFreq(key);
       
-      if(key > 0)
-      	 Write_char_LCD(0,key);
-      switch(key) {
-      	 case -1:
-      	    break;
-	 
-      	    /* DUTY CYCLE */
-      	 case '*':
-      	    waveDuty = (waveDuty-1 >0) ? waveDuty-1 : 1;
-	    while(scanKeypad() != -1);
-      	    break;
-      	 case '0':
-      	    waveDuty=5;
-      	    break;
-      	 case '#':
-      	    waveDuty = (waveDuty <9) ? waveDuty+1 : 9;
-	    while(scanKeypad() != -1);
-      	    break;
 
-
-
-      	    /* Wave Types */
-      	 case '6':
-      	    waveType = 3;	/* Arb Wave */
-	    Clear_LCD();
-	    Write_str_LCD(16,"Type: Arb");
-      	    break;
-      	 case '7':
-      	    waveDuty=5;
-      	    waveType = 0;	/* Square */
-	    Clear_LCD();
-	    Write_str_LCD(16,"Type: Square");
-	    
-      	    break;
-      	 case '8':
-      	    waveType = 1;	/* Sawtooth */
-	    Clear_LCD();
-	    Write_str_LCD(16,"Type: Sawtooth");
-
-      	    break;
-      	 case '9':
-      	    waveType = 2;	/* Sine */
-	    Clear_LCD();
-	    Write_str_LCD(16,"Type: Sine");
-      	    break;
-
-      	 default:
-      	    waveFreq = key -'0';
-      	    break;
-      }
+      /* wait till the key is released */
+      while(scanKeypad() != -1);
 
    }
 
@@ -119,37 +82,25 @@ volatile void main(void)
 volatile void TA0_0_IRQHandler(void) {
    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
    static int index =0;
-
-   
-   if( waveType == 2) {
-      setDAC(SIN_WAVE[index]);
-
-      index = (index + waveFreq) % SIN_LEN;
+   static int skip = 0;		/* used to skip every other sample for square wave */
+   if(waveType != SQUARE_WT) {
+      index = (index + waveFreq) % WAVE_LEN;
+      setDAC(WAVES[waveType][index]);
       TIMER_A0->CCR[0] += CLK_STEP;
-      return;
-   } else if(waveType== 1) {
-      
-      setDAC(index*4096/SIN_LEN);
-      index = (index + waveFreq) % SIN_LEN;
-      TIMER_A0->CCR[0] += CLK_STEP;
-      return;
-   } else if(waveType== 3) {
-      
-      setDAC(index*4096/SIN_LEN);
-      index = (index - waveFreq) % SIN_LEN;
-      TIMER_A0->CCR[0] += CLK_STEP;
-      return;
-   } else if(waveType== 0) {
+   } else if(waveType== SQUARE_WT) {
       /* SQUARE WAVE */
-      index = index ? 0:4095;
-      setDAC( index);
+      if(skip == 0) {
+      	 index = index ? 0:4095;
+      	 setDAC( index);
+      }
+      skip ^= 0x1;
       
-      TIMER_A0->CCR[0] +=  (index? (60000 / waveFreq *waveDuty/5) :
-			    (60000 / waveFreq *(10-waveDuty)/5));
-
+      TIMER_A0->CCR[0] +=  30000 / waveFreq *(index? ( waveDuty/5.0) :
+					      ((10-waveDuty)/5.0));
    }
-
 }
+
+
 
 void setupDAC(){
    P1->SEL0 |=  (BIT5 | BIT6 | BIT7); /* SPI def, pg 134 in datasheet */
@@ -177,4 +128,83 @@ void setDAC(uint16_t data) {
    EUSCI_B0->TXBUF = 0xff & data;
 
    P4->OUT |= (BIT1);
+}
+
+
+
+void updateTypeAndFreq(int key) {
+   switch(key) {
+      case -1:
+	 break;
+	 
+	 /* DUTY CYCLE */
+      case '*':
+	 waveDuty = (waveDuty-1 >0) ? waveDuty-1 : 1;
+	 break;
+      case '0':
+	 waveDuty=5;
+	 break;
+      case '#':
+	 waveDuty = (waveDuty <9) ? waveDuty+1 : 9;
+	 break;
+
+	 /* Wave Types */
+      case '6':
+	 if(waveType >= 3) {
+	    waveType = 4;
+	 } else {
+	    waveType = ARB_WT;	/* Arb Wave */
+	 }
+	 Clear_LCD();
+	 Write_str_LCD(16,"Type: Arb");
+	 break;
+      case '7':
+	 waveDuty=5;
+	 waveType = SQUARE_WT;	/* Square */
+	 Clear_LCD();
+	 Write_str_LCD(16,"Type: Square");
+	    
+	 break;
+      case '8':
+	 waveType = SAWTOOTH_WT;	/* Sawtooth */
+	 Clear_LCD();
+	 Write_str_LCD(16,"Type: Sawtooth");
+
+	 break;
+      case '9':
+	 waveType = SINE_WT;	/* Sine */
+	 Clear_LCD();
+	 Write_str_LCD(16,"Type: Sine");
+	 break;
+
+      default:
+	 waveFreq = key -'0';
+	 break;
+   }
+}
+
+
+int abs(int a) {
+   return( (a>0)? a : -a);
+}
+
+void generateArrays() {
+   int peak=4095;
+   int i;
+   for( i=0; i<WAVE_LEN; i++) {
+      SQUARE_WAVE[i] = (i<WAVE_LEN/2)? 0: peak;
+      SIN_WAVE[i] = (int) (peak/2 + peak/2 * sin(i* 2* PI/WAVE_LEN));
+      SAW_WAVE[i] = (int) (i*peak/WAVE_LEN);
+
+      TRI_WAVE[i] = (int)(peak*(1-2.0/WAVE_LEN*abs(i-WAVE_LEN/2)));
+
+      //(int) (i-2/WAVE_LEN* abs((int)(i-WAVE_LEN/2)));
+      /* NOISE_WAVE[i] = random() * peak; */
+   }
+
+   WAVES[0] = SQUARE_WAVE;
+   WAVES[1] = SAW_WAVE;
+   WAVES[2] = SIN_WAVE;
+   WAVES[3] = NOISE_WAVE;
+   WAVES[4] = TRI_WAVE;
 }
